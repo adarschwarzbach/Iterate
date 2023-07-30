721a5290d0df15d4bd3f05a3abd8334fc9f9db89
adarschwarzbach <adar.schwarzbach@gmail.com>
2023-07-30 09:39:10 -0700

    initial commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,12 @@
 /*!
 
 =========================================================
-* Black Dashboard React v1.2.2
+* BLK Design System React - v1.2.2
 =========================================================
 
-* Product Page: https://www.creative-tim.com/product/black-dashboard-react
+* Product Page: https://www.creative-tim.com/product/blk-design-system-react
 * Copyright 2023 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
+* Licensed under MIT (https://github.com/creativetimofficial/blk-design-system-react/blob/main/LICENSE.md)
 
 * Coded by Creative Tim
 
@@ -15,47 +15,119 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-/*eslint-disable*/
 import React from "react";
-
+import { Link } from "react-router-dom";
 // reactstrap components
-import { Container, Nav, NavItem, NavLink } from "reactstrap";
+import {
+  Button,
+  NavItem,
+  NavLink,
+  Nav,
+  Container,
+  Row,
+  Col,
+  UncontrolledTooltip,
+} from "reactstrap";
 
-function Footer() {
+export default function Footer() {
   return (
     <footer className="footer">
-      <Container fluid>
-        <Nav>
-          <NavItem>
-            <NavLink href="https://www.creative-tim.com/?ref=bdr-user-archive-footer">
-              Creative Tim
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="https://www.creative-tim.com/presentation?ref=bdr-user-archive-footer">
-              About Us
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="https://www.creative-tim.com/blog?ref=bdr-user-archive-footer">
-              Blog
-            </NavLink>
-          </NavItem>
-        </Nav>
-        <div className="copyright">
-          © {new Date().getFullYear()} made with{" "}
-          <i className="tim-icons icon-heart-2" /> by{" "}
-          <a
-            href="https://www.creative-tim.com/?ref=bdr-user-archive-footer"
-            target="_blank"
-          >
-            Creative Tim
-          </a>{" "}
-          for a better web.
-        </div>
+      <Container>
+        <Row>
+          <Col md="3">
+            <h1 className="title">BLK•</h1>
+          </Col>
+          <Col md="3">
+            <Nav>
+              <NavItem>
+                <NavLink to="/" tag={Link}>
+                  Home
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="/landing-page" tag={Link}>
+                  Landing
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="/register-page" tag={Link}>
+                  Register
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="/profile-page" tag={Link}>
+                  Profile
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Col>
+          <Col md="3">
+            <Nav>
+              <NavItem>
+                <NavLink href="https://creative-tim.com/contact-us?ref=blkdsr-footer">
+                  Contact Us
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://creative-tim.com/about-us?ref=blkdsr-footer">
+                  About Us
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://creative-tim.com/blog?ref=blkdsr-footer">
+                  Blog
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://opensource.org/licenses/MIT">
+                  License
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Col>
+          <Col md="3">
+            <h3 className="title">Follow us:</h3>
+            <div className="btn-wrapper profile">
+              <Button
+                className="btn-icon btn-neutral btn-round btn-simple"
+                color="default"
+                href="https://twitter.com/creativetim"
+                id="tooltip622135962"
+                target="_blank"
+              >
+                <i className="fab fa-twitter" />
+              </Button>
+              <UncontrolledTooltip delay={0} target="tooltip622135962">
+                Follow us
+              </UncontrolledTooltip>
+              <Button
+                className="btn-icon btn-neutral btn-round btn-simple"
+                color="default"
+                href="https://www.facebook.com/creativetim"
+                id="tooltip230450801"
+                target="_blank"
+              >
+                <i className="fab fa-facebook-square" />
+              </Button>
+              <UncontrolledTooltip delay={0} target="tooltip230450801">
+                Like us
+              </UncontrolledTooltip>
+              <Button
+                className="btn-icon btn-neutral btn-round btn-simple"
+                color="default"
+                href="https://dribbble.com/creativetim"
+                id="tooltip318450378"
+                target="_blank"
+              >
+                <i className="fab fa-dribbble" />
+              </Button>
+              <UncontrolledTooltip delay={0} target="tooltip318450378">
+                Follow us
+              </UncontrolledTooltip>
+            </div>
+          </Col>
+        </Row>
       </Container>
     </footer>
   );
 }
-
-export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 /*!
 
 =========================================================
-* Black Dashboard React v1.2.2
+* BLK Design System React - v1.2.2
 =========================================================
 
-* Product Page: https://www.creative-tim.com/product/black-dashboard-react
+* Product Page: https://www.creative-tim.com/product/blk-design-system-react
 * Copyright 2023 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
+* Licensed under MIT (https://github.com/creativetimofficial/blk-design-system-react/blob/main/LICENSE.md)
 
 * Coded by Creative Tim
 
@@ -19,32 +19,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
-import AdminLayout from "layouts/Admin/Admin.js";
-import RTLLayout from "layouts/RTL/RTL.js";
-
-import "assets/scss/black-dashboard-react.scss";
-import "assets/demo/demo.css";
 import "assets/css/nucleo-icons.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
+import "assets/scss/blk-design-system-react.scss";
+import "assets/demo/demo.css";
 
-import ThemeContextWrapper from "./components/ThemeWrapper/ThemeWrapper";
-import BackgroundColorWrapper from "./components/BackgroundColorWrapper/BackgroundColorWrapper";
+import Index from "views/Index.js";
+import LandingPage from "views/examples/LandingPage.js";
+import RegisterPage from "views/examples/RegisterPage.js";
+import ProfilePage from "views/examples/ProfilePage.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <ThemeContextWrapper>
-    <BackgroundColorWrapper>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/admin/*" element={<AdminLayout />} />
-          <Route path="/rtl/*" element={<RTLLayout />} />
-          <Route
-            path="*"
-            element={<Navigate to="/admin/dashboard" replace />}
-          />
-        </Routes>
-      </BrowserRouter>
-    </BackgroundColorWrapper>
-  </ThemeContextWrapper>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/components" element={<Index />} />
+      <Route path="/landing-page" element={<LandingPage />} />
+      <Route path="/register-page" element={<RegisterPage />} />
+      <Route path="/profile-page" element={<ProfilePage />} />
+      <Route path="*" element={<Navigate to="/components" replace />} />
+    </Routes>
+  </BrowserRouter>
 );
